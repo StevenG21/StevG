b25716f14b0c0d524e5c0c0efbabe5888c63653d
Brayan <brayan.gomezc@udea.edu.co>
2023-03-27 10:35:39 -0500

    .

---
+++ Practica2/problema4.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+int convert(char* c){
+    int num = 0;
+    
+    while(*c != 0){ //Mientras que no sea el caracter nulo
+        num = num*10 + (*c - 48);  // Cero: 48 en ascii
+        c++;
+    }
+    return num;
+}
+
+
+int main()
+{
+    int n;
+    char* cadena = new char;
+    cout<<"Ingrese una cadena de numeros: ";
+    cin.getline(cadena,100);
+n= convert(cadena);
+cout << n;
+    return 0;
+}
